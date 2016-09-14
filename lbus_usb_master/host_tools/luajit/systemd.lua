c1f9b1bd56e896fac614204a36e8a671268b94aa
Hans-Werner Hilse <hwhilse@gmail.com>
2016-09-14 23:03:56 +0200

    LBUS/usb_master/host/luajit: bugfix systemd lib name

--- lbus_usb_master/host_tools/luajit/systemd.lua
+++ lbus_usb_master/host_tools/luajit/systemd.lua
@@ -4,7 +4,7 @@ ffi.cdef[[
 int sd_notify(int unset_environment, const char *state);
 ]]
 
-local libsystemd = ffi.load("libsystemd.so")
+local libsystemd = ffi.load("libsystemd.so.0")
 local SD = {
 	notify = libsystemd.sd_notify,
 }
